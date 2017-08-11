2981d132dbf5decf32841220c5084b0de88f1159
Eli Heuer <eliheuer@gmail.com>
2017-08-11 13:51:15 -0400

    working on basic script

--- bahai-date.py
+++ bahai-date.py
@@ -2,73 +2,14 @@ import datetime
 
 # class Calendar(object):
 
-now = datetime.datetime.now()
+now = datetime.datetime.today()
 
 print("{:%H:%M:%S}".format(now))
+print("{:%H:%M}".format(now))
+print("{:%H}".format(now))
+print(now.hour)
+print(now.year)
+print(now)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+# Day-name, Month-name, Day-#, Year
